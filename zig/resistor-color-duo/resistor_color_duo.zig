594adfa2dc4f2fe40ba7112171b591303e00d519
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-23 11:30:17 +0700

    using enum and int from enum to solve restior color

--- zig/resistor-color-duo/resistor_color_duo.zig
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -1,34 +1,20 @@
 const fmt = @import("std").fmt;
-pub const ColorBand = enum {
-    black,
-    brown,
-    red,
-    orange,
-    yellow,
-    green,
-    blue,
-    violet,
-    grey,
-    white,
+pub const ColorBand = enum(u8) {
+    black = 0,
+    brown = 1,
+    red = 2,
+    orange = 3,
+    yellow = 4,
+    green = 5,
+    blue = 6,
+    violet = 7,
+    grey = 8,
+    white = 9,
 };
 
-fn getColorVal(color: ColorBand) usize {
-    return switch (color) {
-        ColorBand.black => 0,
-        ColorBand.brown => 1,
-        ColorBand.red => 2,
-        ColorBand.orange => 3,
-        ColorBand.yellow => 4,
-        ColorBand.green => 5,
-        ColorBand.blue => 6,
-        ColorBand.violet => 7,
-        ColorBand.grey => 8,
-        ColorBand.white => 9,
-    };
-}
-
 pub fn colorCode(colors: [2]ColorBand) usize {
-    const val1 = getColorVal(colors[0]);
-    const val2 = getColorVal(colors[1]);
-    return val1 * 10 + val2;
+    const color_val_1 = @intFromEnum(colors[0]);
+    const color_val_2 = @intFromEnum(colors[1]);
+
+    return color_val_1 * 10 + color_val_2;
 }
