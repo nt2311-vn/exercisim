136d89df301b960a7b6466ae5dc96d03ce116bf5
ntnobara <nguyentoan231196@gmail.com>
2024-09-24 20:20:20 +0700

    another way to solve pangram

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -1,14 +1,31 @@
-const ascii = @import("std").ascii;
 const std = @import("std");
 
+// pub fn isPangram(str: []const u8) bool {
+//     var set = std.bit_set.IntegerBitSet(26).initEmpty();
+//     for (str) |c| {
+//         if (!ascii.isAlphabetic(c)) {
+//             continue;
+//         }
+//         set.set(ascii.toLower(c) - 'a');
+//     }
+//
+//     return set.count() == 26;
+// }
+
 pub fn isPangram(str: []const u8) bool {
-    var set = std.bit_set.IntegerBitSet(26).initEmpty();
+    var seen = [_]bool{false} ** 26;
     for (str) |c| {
-        if (!ascii.isAlphabetic(c)) {
-            continue;
+        if (std.ascii.isAlphabetic(c)) {
+            const lower = std.ascii.toLower(c);
+            seen[lower - 'a'] = true;
         }
-        set.set(ascii.toLower(c) - 'a');
     }
 
-    return set.count() == 26;
+    for (seen) |is_seen| {
+        if (!is_seen) {
+            return false;
+        }
+    }
+
+    return true;
 }
