4be5a59e90593bb1db9c61c8fe447d84d0eb8931
ntnobara <nguyentoan231196@gmail.com>
2024-09-24 20:29:22 +0700

    another way of writing using if else as expression

--- zig/leap/leap.zig
+++ zig/leap/leap.zig
@@ -1,12 +1,24 @@
+// pub fn isLeapYear(year: u32) bool {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0) {
+//             if (year % 400 == 0) {
+//                 return true;
+//             }
+//             return false;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
+
 pub fn isLeapYear(year: u32) bool {
     if (year % 4 == 0) {
         if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            }
-            return false;
+            return if (year % 400 == 0) true else false;
+        } else {
+            return true;
         }
-        return true;
     }
+
     return false;
 }
