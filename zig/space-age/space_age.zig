1b8c7ed64aad339395257af67d2499238344c5ee
ntnobara <nguyentoan231196@gmail.com>
2024-09-25 22:08:07 +0700

    submit other iterations

--- zig/space-age/space_age.zig
+++ zig/space-age/space_age.zig
@@ -10,15 +10,17 @@ pub const Planet = enum {
 
     pub fn age(self: Planet, seconds: usize) f64 {
         const earth_years: f64 = @as(f64, @floatFromInt(seconds)) / 31_557_000.0;
-        switch (self) {
-            Planet.mercury => return earth_years / 0.2408467,
-            Planet.venus => return earth_years / 0.61519726,
-            Planet.earth => return earth_years / 1.0,
-            Planet.mars => return earth_years / 1.8808158,
-            Planet.jupiter => return earth_years / 11.862615,
-            Planet.saturn => return earth_years / 29.447498,
-            Planet.uranus => return earth_years / 84.016846,
-            Planet.neptune => return earth_years / 164.79132,
-        }
+        const orbits: f64 = switch (self) {
+            Planet.mercury => 0.2408467,
+            Planet.venus => 0.61519726,
+            Planet.earth => 1.0,
+            Planet.mars => 1.8808158,
+            Planet.jupiter => 11.862615,
+            Planet.saturn => 29.447498,
+            Planet.uranus => 84.016846,
+            Planet.neptune => 164.79132,
+        };
+
+        return earth_years / orbits;
     }
 };
