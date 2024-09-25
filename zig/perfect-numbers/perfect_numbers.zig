48c877e456533b754f2578d93b7a0f06ecba45f6
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-25 10:11:34 +0700

    solved perfect numbers

---
+++ zig/perfect-numbers/perfect_numbers.zig
@@ -0,0 +1,22 @@
+pub const Classification = enum {
+    deficient,
+    perfect,
+    abundant,
+};
+
+/// Asserts that `n` is nonzero.
+pub fn classify(n: u64) Classification {
+    const half: u64 = n / 2;
+    var sum: u64 = 0;
+    for (1..half + 1) |num| {
+        if (n % num == 0) {
+            sum += num;
+        }
+    }
+
+    if (sum == n) {
+        return Classification.perfect;
+    }
+
+    return if (sum > n) Classification.abundant else Classification.deficient;
+}
