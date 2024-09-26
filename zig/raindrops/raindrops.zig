f65657e0f0d38b2b63d497f6aa6613698d5412ed
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-26 10:50:08 +0700

    solved raindrops with bufprint

--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -1,10 +1,18 @@
 const std = @import("std");
 pub fn convert(buffer: []u8, n: u32) []const u8 {
-    var stream = std.io.fixedBufferStream(buffer);
-    if (n % 3 == 0) stream.writer().writeAll("Pling") catch unreachable;
-    if (n % 5 == 0) stream.writer().writeAll("Plang") catch unreachable;
-    if (n % 7 == 0) stream.writer().writeAll("Plong") catch unreachable;
+    // var stream = std.io.fixedBufferStream(buffer);
+    // if (n % 3 == 0) stream.writer().writeAll("Pling") catch unreachable;
+    // if (n % 5 == 0) stream.writer().writeAll("Plang") catch unreachable;
+    // if (n % 7 == 0) stream.writer().writeAll("Plong") catch unreachable;
+    //
+    // if (stream.pos == 0) stream.writer().print("{d}", .{n}) catch unreachable;
+    // return stream.getWritten();
 
-    if (stream.pos == 0) stream.writer().print("{d}", .{n}) catch unreachable;
-    return stream.getWritten();
+    const out = std.fmt.bufPrint(buffer, "{s}{s}{s}", .{
+        if (n % 3 == 0) "Pling" else "",
+        if (n % 5 == 0) "Plang" else "",
+        if (n % 7 == 0) "Plong" else "",
+    }) catch unreachable;
+
+    return if (out.len > 0) out else std.fmt.bufPrint(buffer, "{d}", .{n}) catch unreachable;
 }
