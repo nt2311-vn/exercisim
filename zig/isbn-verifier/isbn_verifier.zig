33ac3f8c2ee4a23c66287660b307693d1fb2100b
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-07 15:57:00 +0700

    solved isbn verifier using for switch

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -1,31 +1,24 @@
 const ascii = @import("std").ascii;
 
 pub fn isValidIsbn10(s: []const u8) bool {
-    var total: u32 = 0;
-    var digit_count: u32 = 0;
-    var multiply: u32 = 10;
+    if (s.len < 10) return false;
 
-    for (s) |char| {
-        if (char == '-') continue;
-        if (digit_count == 9) {
-            if (char == 'X') {
-                total += 10;
-            } else if (ascii.isDigit(char)) {
-                total += @as(u32, @intCast(char - '0'));
-            } else {
-                return false;
-            }
-            digit_count += 1;
-        } else if (digit_count < 9) {
-            if (!ascii.isDigit(char)) return false;
-            const num = @as(u32, @intCast(char - '0'));
-            total += num * multiply;
-            multiply -= 1;
-            digit_count += 1;
-        } else {
-            return false;
-        }
-    }
+    var sum: usize = 0;
+    var index: usize = 10;
 
-    return digit_count == 10 and total % 11 == 0;
+    for (s) |c| switch (c) {
+        '0'...'9' => {
+            const num = c - '0';
+            sum += index * @as(usize, @intCast(num));
+            if (index == 0) return false;
+            index -= 1;
+        },
+        '-' => {},
+        'X' => {
+            sum += 10;
+        },
+        else => return false,
+    };
+
+    return sum % 11 == 0;
 }
