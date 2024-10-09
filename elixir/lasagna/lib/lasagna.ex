bba6dadb31bf9b8c6ff16c46229a8ab7c78775c3
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-09 13:30:37 +0700

    elixir tried

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,9 +1,18 @@
 defmodule Lasagna do
   # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven() do
+    40
+  end
 
   # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(in_oven) do
+    40 - in_oven
+  end
 
   # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layer) do
+    2 * layer
+  end
 
   # Please define the 'total_time_in_minutes/2' function
 
