f721fb328479fe3993d3200ed193ff7efe46f6fc
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-11 16:58:37 +0700

    solved the proverb challenges

--- zig/proverb/proverb.zig
+++ zig/proverb/proverb.zig
@@ -3,23 +3,28 @@ const mem = std.mem;
 const fmt = std.fmt;
 
 pub fn recite(allocator: mem.Allocator, words: []const []const u8) (fmt.AllocPrintError || mem.Allocator.Error)![][]u8 {
-    const rs: [][]u8 = try allocator.alloc([]u8, words.len);
-    defer allocator.free(rs);
+    var rs = std.ArrayList([]u8).init(allocator);
+    defer rs.deinit();
 
-    if (words.len == 0) return rs;
-
+    if (words.len == 0) return rs.toOwnedSlice();
     if (words.len == 1) {
-        fmt.bufPrint(rs[0], "And all for the want of a {s}.\n", .{words[0]});
+        const line = try fmt.allocPrint(allocator, "And all for the want of a {s}.\n", .{words[0]});
+
+        try rs.append(line);
+        return rs.toOwnedSlice();
     }
 
     for (words, 0..) |word, i| {
         if (i == words.len - 1) {
-            fmt.bufPrint(rs[i], "And all for the want of a {s}.\n", .{words[0]});
-            break;
+            const line = try fmt.allocPrint(allocator, "And all for the want of a {s}.\n", .{words[0]});
+            try rs.append(line);
         }
 
-        fmt.bufPrint(rs[i], "For want of a {s} the {s} was lost.\n", .{ word, words[i + 1] });
+        if (i < words.len - 1) {
+            const line = try fmt.allocPrint(allocator, "For want of a {s} the {s} was lost.\n", .{ word, words[i + 1] });
+            try rs.append(line);
+        }
     }
 
-    return rs;
+    return rs.toOwnedSlice();
 }
