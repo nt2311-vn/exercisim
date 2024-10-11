c3453837790071a6732b0e52dc6e4e509f4f171e
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-11 09:38:54 +0700

    solved darts, simplify values

--- zig/darts/darts.zig
+++ zig/darts/darts.zig
@@ -1,13 +1,13 @@
 const math = @import("std").math;
 pub const Coordinate = struct {
-    x_coord: f32,
-    y_coord: f32,
+    x: f32,
+    y: f32,
 
     pub fn init(x_coord: f32, y_coord: f32) Coordinate {
-        return Coordinate{ .x_coord = x_coord, .y_coord = y_coord };
+        return Coordinate{ .x = x_coord, .y = y_coord };
     }
     pub fn score(self: Coordinate) usize {
-        const rad = math.sqrt(self.x_coord * self.x_coord + self.y_coord * self.y_coord);
+        const rad = math.sqrt(self.x * self.x + self.y * self.y);
 
         if (rad <= 1) return 10;
         if (rad <= 5) return 5;
