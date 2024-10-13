cfec38da7bc3f06c68559addc46da5e37c97f9e1
ntnobara <nguyentoan231196@gmail.com>
2024-10-13 23:28:26 +0700

    use pipeline operator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -9,15 +9,13 @@ defmodule KitchenCalculator do
   def to_milliliter({:tablespoon, vol}), do: {:milliliter, vol * 15}
   def to_milliliter({:milliliter, vol}), do: {:milliliter, vol}
 
-  def from_milliliter({_, vol}, :cup), do: {:cup, vol / 240}
-  def from_milliliter({_, vol}, :fluid_ounce), do: {:fluid_ounce, vol / 30}
-  def from_milliliter({_, vol}, :tablespoon), do: {:tablespoon, vol / 15}
-  def from_milliliter({_, vol}, :teaspoon), do: {:teaspoon, vol / 5}
-  def from_milliliter({_, vol}, :milliliter), do: {:milliliter, vol}
+  def from_milliliter({:milliliter, vol}, :cup), do: {:cup, vol / 240}
+  def from_milliliter({:milliliter, vol}, :fluid_ounce), do: {:fluid_ounce, vol / 30}
+  def from_milliliter({:milliliter, vol}, :tablespoon), do: {:tablespoon, vol / 15}
+  def from_milliliter({:milliliter, vol}, :teaspoon), do: {:teaspoon, vol / 5}
+  def from_milliliter({:milliliter, vol}, :milliliter), do: {:milliliter, vol}
 
   def convert(volume_pair, unit) do
-    {_, mil_vol} = to_milliliter(volume_pair)
-    {unit, convert_vol} = from_milliliter({:milliliter, mil_vol}, unit)
-    {unit, convert_vol}
+    volume_pair |> to_milliliter() |> from_milliliter(unit)
   end
 end
