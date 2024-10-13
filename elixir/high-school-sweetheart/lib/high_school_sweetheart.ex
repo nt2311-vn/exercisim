2d80ce3b9529c78982eb1b16bcca6edd0d98e015
ntnobara <nguyentoan231196@gmail.com>
2024-10-14 01:19:01 +0700

    used pattern matching to destruct list returns

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,7 +1,7 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
     String.trim(name)
-    |> String.at(0)
+    |> String.first()
   end
 
   def initial(name) do
@@ -9,7 +9,8 @@ defmodule HighSchoolSweetheart do
   end
 
   def initials(full_name) do
-    String.split(full_name, " ") |> Enum.map(&initial/1) |> Enum.join(" ")
+    [first, last] = String.split(full_name, " ")
+    "#{initial(first)} #{initial(last)}"
   end
 
   def pair(full_name1, full_name2) do
