d690494c45d9ea0d7f4a485c45d1661a5ed96eea
ntnobara <nguyentoan231196@gmail.com>
2024-10-14 21:53:31 +0700

    solved using recursion

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -5,16 +5,14 @@ defmodule BirdCount do
   def increment_day_count([]), do: [1]
   def increment_day_count([h | t]), do: [h + 1 | t]
 
-  def has_day_without_birds?(list) do
-    Enum.any?(list, fn x -> x == 0 end)
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | t]), do: has_day_without_birds?(t)
 
-  def total(list) do
-    Enum.reduce(list, 0, fn x, acc -> x + acc end)
-  end
+  def total([]), do: 0
+  def total([h | t]), do: h + total(t)
 
-  def busy_days(list) do
-    Enum.filter(list, fn x -> x >= 5 end)
-    |> Enum.count()
-  end
+  def busy_days([]), do: 0
+  def busy_days([h | t]) when h >= 5, do: 1 + busy_days(t)
+  def busy_days([_ | t]), do: busy_days(t)
 end
