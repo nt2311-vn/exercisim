8b1d0f5f5c888a397eb5f9abd820e1f6608cd5f0
ntnobara <nguyentoan231196@gmail.com>
2024-10-14 23:04:37 +0700

    solved eliud-eggs problem using pipeline operator

---
+++ elixir/eliuds-eggs/lib/eliuds_eggs.ex
@@ -0,0 +1,11 @@
+defmodule EliudsEggs do
+  @doc """
+  Given the number, count the number of eggs.
+  """
+  @spec egg_count(number :: integer()) :: non_neg_integer()
+  def egg_count(number) do
+    number
+    |> Integer.digits(2)
+    |> Enum.count(fn x -> x == 1 end)
+  end
+end
