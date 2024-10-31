b241c3b8ac3a8b3c284673731b4ddfda8e421c82
ntnobara <nguyentoan231196@gmail.com>
2024-10-31 22:28:09 +0700

    solved with pattern matching and simple functions utilization

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,8 +1,26 @@
 defmodule ResistorColorDuo do
+  @resitor_color %{
+    :black => 0,
+    :brown => 1,
+    :red => 2,
+    :orange => 3,
+    :yellow => 4,
+    :green => 5,
+    :blue => 6,
+    :violet => 7,
+    :grey => 8,
+    :white => 9
+  }
+
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
+    [first, second] = Enum.take(colors, 2)
+    color_to_value(first) * 10 + color_to_value(second)
   end
+
+  @spec color_to_value(atom()) :: integer()
+  defp color_to_value(color) when is_map_key(@resitor_color, color), do: @resitor_color[color]
 end
