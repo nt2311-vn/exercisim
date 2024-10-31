5a4a8197c362879ab0da9ed98f1a757f30f8bcd1
ntnobara <nguyentoan231196@gmail.com>
2024-10-31 21:21:58 +0700

    try solved resitor color trio, removed exclude pending test packages

---
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -0,0 +1,8 @@
+defmodule ResistorColorTrio do
+  @doc """
+  Calculate the resistance value in ohms from resistor colors
+  """
+  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
+  def label(colors) do
+  end
+end
