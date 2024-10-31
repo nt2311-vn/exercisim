a2ef246fffeca4ccf266eff5392cefb24b76b231
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-31 17:40:24 +0700

    try understand solving problem with pig latin

--- zig/pig-latin/pig_latin.zig
+++ zig/pig-latin/pig_latin.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
 const mem = std.mem;
+const ascii = std.ascii;
+
+fn isVowels(c: u8) bool {
+    return switch (ascii.toLower(c)) {
+        'a', 'i', 'u', 'e', 'o' => true,
+        else => false,
+    };
+}
+
+fn translateTwoLetterYWord(allocator: mem.Allocator, word: []const u8) mem.Allocator.Error![]u8 {
+    var result = try allocator.alloc(u8, word.len + 2);
+    result[0] = word[1];
+    result[1] = word[0];
+
+    @memcpy(result[2..], "ay");
+    return result;
+}
 
 pub fn translate(allocator: mem.Allocator, phrase: []const u8) mem.Allocator.Error![]u8 {
     _ = allocator;
