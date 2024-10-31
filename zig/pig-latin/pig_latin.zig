5117bbcfe73b4d910d78968785cbfbe5bfc2f878
nt2311-vn <nguyentoan231196@gmail.com>
2024-10-31 17:24:52 +0700

    tried solved pig latin with zig

--- zig/pig-latin/pig_latin.zig
+++ zig/pig-latin/pig_latin.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const mem = std.mem;
 
-const vowels = [5]u8{ 'a', 'i', 'u', 'e', 'o' };
-
 pub fn translate(allocator: mem.Allocator, phrase: []const u8) mem.Allocator.Error![]u8 {
     _ = allocator;
     _ = phrase;
