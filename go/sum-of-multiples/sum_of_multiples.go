cb733a7b5e8473adcd7848e710a6c9b12ad2edfb
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-05 11:44:10 +0700

    solved without implement another list of integer

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -2,7 +2,6 @@ package summultiples
 
 func SumMultiples(limit int, divisors ...int) int {
 	setMap := map[int]bool{}
-	setVal := make([]int, 0)
 
 	for _, divisor := range divisors {
 		if divisor == 0 {
@@ -11,14 +10,13 @@ func SumMultiples(limit int, divisors ...int) int {
 		for i := divisor; i < limit; i += divisor {
 			if !setMap[i] {
 				setMap[i] = true
-				setVal = append(setVal, i)
 			}
 		}
 	}
 
 	total := 0
-	for _, num := range setVal {
-		total += num
+	for k := range setMap {
+		total += k
 	}
 
 	return total
