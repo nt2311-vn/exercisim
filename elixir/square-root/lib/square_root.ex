0f2658774af45eee1bc7739b80370fb02ec962b8
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-17 10:50:35 +0700

    solved squared root challenges

--- elixir/square-root/lib/square_root.ex
+++ elixir/square-root/lib/square_root.ex
@@ -9,14 +9,21 @@ defmodule SquareRoot do
     binary_search(0, radicand, radicand)
   end
 
+  @spec binary_search(low :: pos_integer, high :: pos_integer, num :: pos_integer) ::
+          pos_integer
   defp binary_search(low, high, num) do
-    mid = div(low + high, 2)
+    mid = low + div(high - low, 2)
     square = mid * mid
 
-    cond do
-      square == num -> mid
-      square < num -> binary_search(mid + 1, high, num)
-      square > num -> binary_search(low, mid - 1, num)
+    case square do
+      _ when num == square ->
+        mid
+
+      _ when square < num ->
+        binary_search(mid + 1, high, num)
+
+      _ ->
+        binary_search(low, mid - 1, num)
     end
   end
 end
