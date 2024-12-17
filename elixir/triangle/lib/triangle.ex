8f750459e8923f866af752dd0338787f62c29714
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-17 11:35:17 +0700

    solved triangle another approaches

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -6,17 +6,16 @@ defmodule Triangle do
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
   def kind(a, b, c) do
-    check = is_triangle(a, b, c)
-    if check == nil, do: triangle_kind(a, b, c), else: is_triangle(a, b, c)
+    cond do
+      Enum.any?([a <= 0, b <= 0, c <= 0]) -> {:error, "all side lengths must be positive"}
+      !proper_lengths?(a, b, c) -> {:error, "side lengths violate triangle inequality"}
+      true -> triangle_kind(a, b, c)
+    end
   end
 
-  @spec is_triangle(number, number, number) :: {:error, String.t()} | nil
-  defp is_triangle(a, b, c) do
-    cond do
-      a <= 0 or b <= 0 or c <= 0 -> {:error, "all side lengths must be positive"}
-      a + b < c or a + c < b or b + c < a -> {:error, "side lengths violate triangle inequality"}
-      true -> nil
-    end
+  @spec proper_lengths?(number, number, number) :: boolean
+  defp proper_lengths?(a, b, c) do
+    a + b >= c and a + c >= b and b + c >= a
   end
 
   @spec triangle_kind(number, number, number) :: {:ok, kind}
