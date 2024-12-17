aa5b47c73bddde55b45897af150b307f6a8ebdbd
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-17 09:53:43 +0700

    complete the io challenges

---
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,27 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts("Welcome! Let's fill out your character sheet together.")
+  end
+
+  def ask_name() do
+    IO.gets("What is your character's name?\n") |> String.trim()
+  end
+
+  def ask_class() do
+    IO.gets("What is your character's class?\n") |> String.trim()
+  end
+
+  def ask_level() do
+    IO.gets("What is your character's level?\n") |> String.trim() |> String.to_integer()
+  end
+
+  def run() do
+    welcome()
+    name = ask_name()
+    class = ask_class()
+    level = ask_level()
+
+    IO.puts("Your character: " <> inspect(%{name: name, level: level, class: class}))
+    %{name: name, level: level, class: class}
+  end
+end
