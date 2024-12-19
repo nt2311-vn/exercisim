ffe1510369f2f5a89322ccf64a6f0be009170ff9
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-19 14:26:18 +0700

    another approach using pipe operator

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -4,13 +4,17 @@ defmodule Gigasecond do
   """
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
-  def from({{year, month, day}, {hours, minutes, seconds}}) do
-    {:ok, naive_datetime} =
-      NaiveDateTime.new(year, month, day, hours, minutes, seconds)
-
-    giga_after = NaiveDateTime.add(naive_datetime, 1_000_000_000)
-
-    {{giga_after.year, giga_after.month, giga_after.day},
-     {giga_after.hour, giga_after.minute, giga_after.second}}
+  def from(datetime) do
+    # {:ok, naive_datetime} =
+    #   NaiveDateTime.new(year, month, day, hours, minutes, seconds)
+    #
+    # giga_after = NaiveDateTime.add(naive_datetime, 1_000_000_000)
+    #
+    # {{giga_after.year, giga_after.month, giga_after.day},
+    #  {giga_after.hour, giga_after.minute, giga_after.second}}
+    datetime
+    |> NaiveDateTime.from_erl!()
+    |> NaiveDateTime.add(1_000_000_000)
+    |> NaiveDateTime.to_erl()
   end
 end
