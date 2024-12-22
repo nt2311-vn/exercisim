534a14ecc6c0656626ee2b5c320032bea97ac218
nt2311 <nguyentoan231196@gmail.com>
2024-12-22 23:19:45 +0700

    complete with simple reduce

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,15 +1,19 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    do_extract(data, String.split(path, "."))
+    path
+    |> String.split(".")
+    |> Enum.reduce(data, fn k, acc ->
+      if is_map(acc) and Map.has_key?(acc, k), do: acc[k], else: nil
+    end)
   end
 
   def get_in_path(data, path) do
     get_in(data, String.split(path, "."))
   end
 
-  defp do_extract(data, []), do: data
-
-  defp do_extract(data, [head | tail]) do
-    do_extract(data[head], tail)
-  end
+  # defp do_extract(data, []), do: data
+  #
+  # defp do_extract(data, [head | tail]) do
+  #   do_extract(data[head], tail)
+  # end
 end
