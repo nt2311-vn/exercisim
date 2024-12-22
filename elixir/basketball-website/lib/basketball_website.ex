1f89eff7024e12b626fa6147b3e47c4b84994979
nt2311 <nguyentoan231196@gmail.com>
2024-12-22 23:21:32 +0700

    complete the access behavior

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,19 +1,21 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    path
-    |> String.split(".")
-    |> Enum.reduce(data, fn k, acc ->
-      if is_map(acc) and Map.has_key?(acc, k), do: acc[k], else: nil
-    end)
+    # path
+    # |> String.split(".")
+    # |> Enum.reduce(data, fn k, acc ->
+    #   if is_map(acc) and Map.has_key?(acc, k), do: acc[k], else: nil
+    # end)
+    key_args = path |> String.split(".")
+    do_extract(data, key_args)
   end
 
   def get_in_path(data, path) do
     get_in(data, String.split(path, "."))
   end
 
-  # defp do_extract(data, []), do: data
-  #
-  # defp do_extract(data, [head | tail]) do
-  #   do_extract(data[head], tail)
-  # end
+  defp do_extract(data, []), do: data
+
+  defp do_extract(data, [head | tail]) do
+    do_extract(data[head], tail)
+  end
 end
