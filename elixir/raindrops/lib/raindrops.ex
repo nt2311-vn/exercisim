fd1fa337b6a1a6d6b1aa975156d6af6fc635c257
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-25 14:39:35 +0700

    solved the fizzbuzz challenge

---
+++ elixir/raindrops/lib/raindrops.ex
@@ -0,0 +1,23 @@
+defmodule Raindrops do
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @spec convert(pos_integer) :: String.t()
+  def convert(number) do
+    "" |> add_pling(number) |> add_plang(number) |> add_plong(number) |> return_result(number)
+  end
+
+  defp add_pling(x, num), do: if(rem(num, 3) == 0, do: "#{x}Pling", else: x)
+  defp add_plang(x, num), do: if(rem(num, 5) == 0, do: "#{x}Plang", else: x)
+  defp add_plong(x, num), do: if(rem(num, 7) == 0, do: "#{x}Plong", else: x)
+
+  defp return_result(x, num) do
+    if String.length(x) == 0, do: Integer.to_string(num), else: x
+  end
+end
