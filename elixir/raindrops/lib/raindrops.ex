59339a0b78021de8c7a492a31944a8faa9efb8a0
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-25 14:44:22 +0700

    solved by pattern matching all cases

--- elixir/raindrops/lib/raindrops.ex
+++ elixir/raindrops/lib/raindrops.ex
@@ -10,14 +10,29 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t()
   def convert(number) do
-    "" |> add_pling(number) |> add_plang(number) |> add_plong(number) |> return_result(number)
+    # "" |> add_pling(number) |> add_plang(number) |> add_plong(number) |> return_result(number)
+
+    remain_three = rem(number, 3)
+    remain_five = rem(number, 5)
+    remain_seven = rem(number, 7)
+
+    case {remain_three, remain_five, remain_seven} do
+      {0, 0, 0} -> "PlingPlangPlong"
+      {0, 0, _} -> "PlingPlang"
+      {0, _, 0} -> "PlingPlong"
+      {_, 0, 0} -> "PlangPlong"
+      {0, _, _} -> "Pling"
+      {_, 0, _} -> "Plang"
+      {_, _, 0} -> "Plong"
+      _ -> Integer.to_string(number)
+    end
   end
 
-  defp add_pling(x, num), do: if(rem(num, 3) == 0, do: "#{x}Pling", else: x)
-  defp add_plang(x, num), do: if(rem(num, 5) == 0, do: "#{x}Plang", else: x)
-  defp add_plong(x, num), do: if(rem(num, 7) == 0, do: "#{x}Plong", else: x)
-
-  defp return_result(x, num) do
-    if String.length(x) == 0, do: Integer.to_string(num), else: x
-  end
+  # defp add_pling(x, num), do: if(rem(num, 3) == 0, do: "#{x}Pling", else: x)
+  # defp add_plang(x, num), do: if(rem(num, 5) == 0, do: "#{x}Plang", else: x)
+  # defp add_plong(x, num), do: if(rem(num, 7) == 0, do: "#{x}Plong", else: x)
+  #
+  # defp return_result(x, num) do
+  #   if String.length(x) == 0, do: Integer.to_string(num), else: x
+  # end
 end
