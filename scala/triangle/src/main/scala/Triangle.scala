a1326afc047ba7221fa43abeea33517218992dc8
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-26 14:05:55 +0700

    solved with traits and class

--- scala/triangle/src/main/scala/Triangle.scala
+++ scala/triangle/src/main/scala/Triangle.scala
@@ -1,12 +1,29 @@
-case class Triangle(x: Float, y: Float, z: Float):
-  def isTriangle: Boolean =
+trait TriClass:
+  def isTriangle(x: Float, y: Float, z: Float): Boolean =
     x > 0 && y > 0 && z > 0 && x + y >= z && x + z >= y && y + z >= x
 
-  def equilateral: Boolean =
-    isTriangle && x == y && x == z
+  def equilateral(x: Float, y: Float, z: Float): Boolean =
+    isTriangle(x, y, z) && x == y && x == z
 
-  def isosceles: Boolean =
-    isTriangle && (x == y || y == z || z == x)
+  def isosceles(x: Float, y: Float, z: Float): Boolean =
+    isTriangle(x, y, z) && (x == y || y == z || z == x)
 
-  def scalene: Boolean =
-    isTriangle && !equilateral && !isosceles
+  def scalene(x: Float, y: Float, z: Float): Boolean =
+    isTriangle(x, y, z) && !equilateral(x, y, z) && !isosceles(x, y, z)
+
+case class Triangle(x: Float, y: Float, z: Float) extends TriClass:
+  def equilateral: Boolean = super.equilateral(x, y, z)
+  def isosceles: Boolean = super.isosceles(x, y, z)
+  def scalene: Boolean = super.scalene(x, y, z)
+
+  // def isTriangle: Boolean =
+  //   x > 0 && y > 0 && z > 0 && x + y >= z && x + z >= y && y + z >= x
+
+  // def equilateral: Boolean =
+  //   isTriangle && x == y && x == z
+  //
+  // def isosceles: Boolean =
+  //   isTriangle && (x == y || y == z || z == x)
+  //
+  // def scalene: Boolean =
+  //   isTriangle && !equilateral && !isosceles
