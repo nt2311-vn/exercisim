c2db31d87947a412025b58214056820fb1f33b54
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-26 17:22:06 +0700

    solved with case and if guard

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -12,8 +12,14 @@ object Bob:
     msg.trim == ""
 
   def response(msg: String): String =
-    if yellAndQuestion(msg) then "Calm down, I know what I'm doing!"
-    else if yell(msg) then "Whoa, chill out!"
-    else if question(msg) then "Sure."
-    else if silence(msg) then "Fine. Be that way!"
-    else "Whatever."
+    // if yellAndQuestion(msg) then "Calm down, I know what I'm doing!"
+    // else if yell(msg) then "Whoa, chill out!"
+    // else if question(msg) then "Sure."
+    // else if silence(msg) then "Fine. Be that way!"
+    // else "Whatever."
+    msg.trim match
+      case _ if yellAndQuestion(msg) => "Calm down, I know what I'm doing!"
+      case _ if yell(msg)            => "Whoa, chill out!"
+      case _ if question(msg)        => "Sure."
+      case _ if silence(msg)         => "Fine. Be that way!"
+      case _                         => "Whatever."
