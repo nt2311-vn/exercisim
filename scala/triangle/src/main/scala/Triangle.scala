95555e8ec2694dafc750f746359a4a31229659b3
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-26 13:52:43 +0700

    solved Triangle in scala

--- scala/triangle/src/main/scala/Triangle.scala
+++ scala/triangle/src/main/scala/Triangle.scala
@@ -1,3 +1,12 @@
-object Triangle {
-  println("Hello, world!")
-}
+case class Triangle(x: Float, y: Float, z: Float):
+  def isTriangle: Boolean =
+    x > 0 && y > 0 && z > 0 && x + y >= z && x + z >= y && y + z >= x
+
+  def equilateral: Boolean =
+    isTriangle && x == y && x == z
+
+  def isosceles: Boolean =
+    isTriangle && (x == y || y == z || z == x)
+
+  def scalene: Boolean =
+    isTriangle && !equilateral && !isosceles
