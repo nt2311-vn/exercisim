89ed5541e7853cf0621b41f76b0c79a636f04157
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-27 15:50:16 +0700

    solved strain using simple pattern matching

--- scala/strain/src/main/scala/Strain.scala
+++ scala/strain/src/main/scala/Strain.scala
@@ -1,4 +1,18 @@
 object Strain:
-  def keep[A](list: Seq[A], bool: A => Boolean): Seq[A] = list.filter(bool)
-  def discard[A](list: Seq[A], bool: A => Boolean): Seq[A] =
-    list.filterNot(bool)
+  // def keep[A](list: Seq[A], bool: A => Boolean): Seq[A] = list.filter(bool)
+  // def discard[A](list: Seq[A], bool: A => Boolean): Seq[A] =
+  //   list.filterNot(bool)
+
+  def keep[A](seq: Seq[A], func: A => Boolean): Seq[A] =
+    seq match
+      case Nil => Nil
+      case head :: tail =>
+        if func(head) then head +: keep(tail, func)
+        else keep(tail, func)
+
+  def discard[A](seq: Seq[A], func: A => Boolean): Seq[A] =
+    seq match
+      case Nil => Nil
+      case head :: tail =>
+        if func(head) then discard(tail, func)
+        else head +: discard(tail, func)
