c8d2b57dfed4fb65c6d3af232aacdefe12719a48
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-02 16:44:45 +0700

    solved using pattern matching

--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -1,9 +1,19 @@
 object Raindrops:
   def convert(n: Int): String =
-    var s: String = ""
+    // var s: String = ""
 
-    s = if n % 3 == 0 then s"${s}Pling" else s
-    s = if n % 5 == 0 then s"${s}Plang" else s
-    s = if n % 7 == 0 then s"${s}Plong" else s
+    // s = if n % 3 == 0 then s"${s}Pling" else s
+    // s = if n % 5 == 0 then s"${s}Plang" else s
+    // s = if n % 7 == 0 then s"${s}Plong" else s
+    //
+    // if s.length == 0 then n.toString else s
 
-    if s.length == 0 then n.toString else s
+    n match
+      case _ if n % 3 == 0 && n % 5 == 0 && n % 7 == 0 => "PlingPlangPlong"
+      case _ if n % 3 == 0 && n % 5 == 0               => "PlingPlang"
+      case _ if n % 5 == 0 && n % 7 == 0               => "PlangPlong"
+      case _ if n % 3 == 0 && n % 7 == 0               => "PlingPlong"
+      case _ if n % 3 == 0                             => "Pling"
+      case _ if n % 5 == 0                             => "Plang"
+      case _ if n % 7 == 0                             => "Plong"
+      case _                                           => n.toString
