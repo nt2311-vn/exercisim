c3209fb7b5c332e08ed9203b9d864229790e8ad6
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-02 17:53:44 +0700

    write using pipe chaining operator

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,10 +1,16 @@
 object SumOfMultiples:
   def sum(factors: Set[Int], limit: Int): Int =
-    var uniqueMul: Set[Int] = Set()
+    // var uniqueMul: Set[Int] = Set()
 
-    for
-      factor <- factors if factor != 0
-      num <- (1 until limit) if num % factor == 0
-    yield uniqueMul = uniqueMul + num
+    // for
+    //   factor <- factors if factor != 0
+    //   num <- (1 until limit) if num % factor == 0
+    // yield uniqueMul = uniqueMul + num
 
-    uniqueMul.foldLeft(0)(_ + _)
+    // uniqueMul.foldLeft(0)(_ + _)
+
+    factors
+      .filter(_ != 0)
+      .flatMap(factor => (1 until limit).filter(_ % factor == 0))
+      .toSet
+      .foldLeft(0)(_ + _)
