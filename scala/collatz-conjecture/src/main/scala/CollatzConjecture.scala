4f42962c43f8dacdad684deaf723944b6b5eea23
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-03 14:32:27 +0700

    sovled using OOP approach

--- scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,12 +1,25 @@
+// object CollatzConjecture:
+//
+//   def steps(n: Int): Option[Int] =
+//     def findCollat(n: Int, times: Int = 0): Option[Int] =
+//       n match
+//         case _ if n <= 0 => None
+//         case 1           => Some(times)
+//         case _ =>
+//           if n % 2 == 0 then findCollat(n / 2, times + 1)
+//           else findCollat(3 * n + 1, times + 1)
+//
+//     findCollat(n)
+
 object CollatzConjecture:
-
   def steps(n: Int): Option[Int] =
-    def findCollat(n: Int, times: Int = 0): Option[Int] =
-      n match
-        case _ if n <= 0 => None
-        case 1           => Some(times)
-        case _ =>
-          if n % 2 == 0 then findCollat(n / 2, times + 1)
-          else findCollat(3 * n + 1, times + 1)
+    if n <= 0 then None
+    else
+      var collatz = n
+      var times = 0
 
-    findCollat(n)
+      while collatz != 1 do
+        collatz = if collatz % 2 == 0 then collatz / 2 else 3 * collatz + 1
+        times += 1
+
+      Some(times)
