4452be80762edb8c76c2d1aca8bdce6e6b541797
nt2311-vn <nguyentoan231196@gmail.com>
2025-01-03 15:29:27 +0700

    solved with for loop

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,10 +1,20 @@
 object Hamming:
   def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] =
-    val diff = dnaStrandTwo.length - dnaStrandOne.length
-    diff match
-      case 0 =>
-        val dist = dnaStrandTwo
-          .zip(dnaStrandOne)
-          .count({ case (nucTwo, nucOne) => nucTwo != nucOne })
-        Some(dist)
-      case _ => None
+    // val diff = dnaStrandTwo.length - dnaStrandOne.length
+    // diff match
+    //   case 0 =>
+    //     val dist = dnaStrandTwo
+    //       .zip(dnaStrandOne)
+    //       .count({ case (nucTwo, nucOne) => nucTwo != nucOne })
+    //     Some(dist)
+    //   case _ => None
+
+    if dnaStrandOne.length != dnaStrandTwo.length then None
+    else
+      var diff = 0
+      for
+        i <- dnaStrandOne.indices
+        if dnaStrandTwo(i) != dnaStrandOne(i)
+      do diff += 1
+
+      Some(diff)
