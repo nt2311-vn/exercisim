37e191d79043a132e30de8fae0091db81a947de0
nt2311 <nguyentoan231196@gmail.com>
2025-02-10 22:19:13 +0700

    solved the news letter challenge with elixir

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -6,18 +6,24 @@ defmodule Newsletter do
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    File.open!(path, [:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    IO.puts(pid, email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    File.close(pid)
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    email_list = read_emails(emails_path)
+    log_pid = open_log(log_path)
+
+    email_list
+    |> Enum.each(fn email -> if send_fun.(email) == :ok, do: log_sent_email(log_pid, email) end)
+
+    close_log(log_pid)
   end
 end
