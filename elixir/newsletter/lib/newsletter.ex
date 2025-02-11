4d5c02b609f7522b092af893414bc776d65eb485
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-11 11:15:48 +0700

    solved with another approach

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,8 +1,6 @@
 defmodule Newsletter do
   def read_emails(path) do
-    File.read!(path)
-    |> String.split("\n", trim: true)
-    |> Enum.reject(fn x -> x == "" end)
+    File.stream!(path) |> Stream.map(&String.trim/1) |> Enum.to_list()
   end
 
   def open_log(path) do
@@ -18,11 +16,15 @@ defmodule Newsletter do
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    email_list = read_emails(emails_path)
     log_pid = open_log(log_path)
 
-    email_list
-    |> Enum.each(fn email -> if send_fun.(email) == :ok, do: log_sent_email(log_pid, email) end)
+    emails_path
+    |> read_emails()
+    |> Enum.each(fn email ->
+      with :ok <- send_fun.(email) do
+        log_sent_email(log_pid, email)
+      end
+    end)
 
     close_log(log_pid)
   end
