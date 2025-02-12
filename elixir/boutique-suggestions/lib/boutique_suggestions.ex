6a3d9e49aab2b6b94a185306df32838d9442ce3c
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-12 17:50:28 +0700

    solved with a single for loop with multiple condition

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -2,9 +2,11 @@ defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, opts \\ []) do
     maximum_price = Keyword.get(opts, :maximum_price, 100)
 
-    tops
-    |> Enum.flat_map(&Enum.map(bottoms, fn bottom -> {&1, bottom} end))
-    |> Enum.reject(fn {top, bottom} -> top.base_color == bottom.base_color end)
-    |> Enum.filter(fn {top, bottom} -> top.price + bottom.price <= maximum_price end)
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color,
+        top.price + bottom.price <= maximum_price do
+      {top, bottom}
+    end
   end
 end
