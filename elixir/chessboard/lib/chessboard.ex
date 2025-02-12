11ab1d2de44834157dee73aa9faec68fb30976eb
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-12 14:00:16 +0700

    complete solved range with 2nd solution

--- elixir/chessboard/lib/chessboard.ex
+++ elixir/chessboard/lib/chessboard.ex
@@ -8,10 +8,10 @@ defmodule Chessboard do
   end
 
   def ranks do
-    1..8 |> Enum.to_list()
+    rank_range() |> Enum.to_list()
   end
 
   def files do
-    ?A..?H |> Enum.map(&<<&1>>)
+    file_range() |> Enum.map(fn x -> <<x>> end)
   end
 end
