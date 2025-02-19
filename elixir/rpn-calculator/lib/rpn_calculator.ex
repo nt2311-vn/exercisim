38a0ced95f8b2d0abc03685d0b00345b51ffd179
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-19 13:58:23 +0700

    complete rpn matching to know about try rescue in Elixir

---
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -0,0 +1,25 @@
+defmodule RPNCalculator do
+  def calculate!(stack, operation) do
+    try do
+      operation.(stack)
+    rescue
+      e -> raise RuntimeError, message: "Calculation failed: #{Exception.message(e)}"
+    end
+  end
+
+  def calculate(stack, operation) do
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      _ -> :error
+    end
+  end
+
+  def calculate_verbose(stack, operation) do
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      e -> {:error, Exception.message(e)}
+    end
+  end
+end
