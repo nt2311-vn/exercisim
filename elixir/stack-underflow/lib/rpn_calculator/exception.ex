aa8eaad2ca2b7f7ed120b775112295ceec275889
nt2311-vn <nguyentoan231196@gmail.com>
2025-02-19 16:15:12 +0700

    solved with execption

---
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -0,0 +1,24 @@
+defmodule RPNCalculator.Exception do
+  # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
+
+  # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    @impl true
+    def exception(value) do
+      case value do
+        [] -> %StackUnderflowError{}
+        _ -> %StackUnderflowError{message: "stack underflow occurred, context: " <> value}
+      end
+    end
+  end
+
+  def divide([]), do: raise(StackUnderflowError, "when dividing")
+  def divide([_]), do: raise(StackUnderflowError, "when dividing")
+  def divide([0 | _]), do: raise(DivisionByZeroError)
+  def divide([a, b]), do: b / a
+end
