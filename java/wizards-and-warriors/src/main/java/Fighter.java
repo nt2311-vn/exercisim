00f911cb9812c22dd29403679c138b1912c590dc
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-13 14:16:37 +0700

    clone exercise inheritance

---
+++ java/wizards-and-warriors/src/main/java/Fighter.java
@@ -0,0 +1,14 @@
+class Fighter {
+
+    boolean isVulnerable() {
+        return true;
+    }
+
+    int getDamagePoints(Fighter fighter) {
+        return 1;
+    }
+}
+
+// TODO: define the Warrior class
+
+// TODO: define the Wizard class
