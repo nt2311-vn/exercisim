0642c07d049fe0b5ad37f3264ea9d4d84fa55f79
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-13 09:41:22 +0700

    clone the problem need for speed

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,31 @@
+class NeedForSpeed {
+    NeedForSpeed(int speed, int batteryDrain) {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+    }
+
+    public boolean batteryDrained() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+    }
+
+    public int distanceDriven() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+    }
+
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+    }
+
+    public static NeedForSpeed nitro() {
+        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+    }
+}
+
+class RaceTrack {
+    RaceTrack(int distance) {
+        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        throw new UnsupportedOperationException("Please implement the RaceTrack.canFinishRace() method");
+    }
+}
