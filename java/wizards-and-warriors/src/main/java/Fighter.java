ca8ed667bd9498c0c0b17f1dba4c5a4825c8cfcc
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-13 14:37:59 +0700

    solved exercise of inheritance

--- java/wizards-and-warriors/src/main/java/Fighter.java
+++ java/wizards-and-warriors/src/main/java/Fighter.java
@@ -1,14 +1,69 @@
 class Fighter {
 
-    boolean isVulnerable() {
-        return true;
-    }
+  boolean isVulnerable() {
+    return true;
+  }
 
-    int getDamagePoints(Fighter fighter) {
-        return 1;
-    }
+  int getDamagePoints(Fighter fighter) {
+    return 1;
+  }
+
+  public String toString() {
+    return "Fighter is Fighter";
+  }
 }
 
 // TODO: define the Warrior class
+class Warrior extends Fighter {
+
+  @Override
+  public String toString() {
+    return "Fighter is a Warrior";
+  }
+
+  @Override
+  boolean isVulnerable() {
+    return false;
+  }
+
+  @Override
+  int getDamagePoints(Fighter fighter) {
+    if (fighter.isVulnerable()) {
+      return 10;
+    }
+
+    return 6;
+  }
+}
 
 // TODO: define the Wizard class
+class Wizard extends Fighter {
+  private boolean preparedSpell = false;
+
+  void prepareSpell() {
+    this.preparedSpell = true;
+  }
+
+  @Override
+  public String toString() {
+    return "Fighter is a Wizard";
+  }
+
+  @Override
+  boolean isVulnerable() {
+    if (this.preparedSpell) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  int getDamagePoints(Fighter fighter) {
+    if (this.preparedSpell) {
+      return 12;
+    }
+
+    return 3;
+  }
+}
