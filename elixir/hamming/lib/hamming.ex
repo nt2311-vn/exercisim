f2a9cb744381d7c872557e54a05c8096bc020dcd
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-10 21:39:08 +0700

    complete the hamming distance using Enum.with_index like a normal loop

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -8,6 +8,24 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
-  def hamming_distance(strand1, strand2) do
+  def hamming_distance([], []), do: {:ok, 0}
+  def hamming_distance(base, base), do: {:ok, 0}
+
+  def hamming_distance(base, compare) when length(base) != length(compare),
+    do: {:error, "strands must be of equal length"}
+
+  def hamming_distance(base, compare) do
+    distance =
+      base
+      |> Enum.with_index()
+      |> Enum.reduce(0, fn {base_strand, idx}, acc ->
+        if base_strand == Enum.at(compare, idx) do
+          acc
+        else
+          acc + 1
+        end
+      end)
+
+    {:ok, distance}
   end
 end
