1b20663c1406ae85ef89d5fd8697df9a3d277ea4
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-10 11:10:25 +0700

    complete solving of sum of multiple

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,19 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    new_list = []
+
+    factors
+    |> Enum.reduce(new_list, fn factor, list ->
+      cond do
+        factor >= limit or factor == 0 ->
+          list
+
+        true ->
+          list ++ for n <- factor..(limit - 1), rem(n, factor) == 0, do: n
+      end
+    end)
+    |> Enum.uniq()
+    |> Enum.sum()
   end
 end
