e77c469950306a8e475c27f1eaadc2bc050792fc
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-10 10:21:47 +0700

    solved arm strong number

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -5,5 +5,14 @@ defmodule ArmstrongNumber do
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
+    num_str = Integer.to_string(number)
+    str_len = String.length(num_str)
+
+    number ==
+      num_str
+      |> String.graphemes()
+      |> Enum.reduce(0, fn number, total ->
+        total + Integer.pow(String.to_integer(number), str_len)
+      end)
   end
 end
