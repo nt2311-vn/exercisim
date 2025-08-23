231885d27742377b1c5dabdee3a575dc306908d3
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-23 10:25:35 +0700

    solved leap year

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -5,5 +5,5 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-    return year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    return year % 400 == 0 ||  year % 4 == 0 && year % 100 != 0
 end
