31554dde3f27f2130107280ee1f44abd1169845f
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-23 15:45:39 +0700

    complete rainsdrop by matching all case

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,17 +1,20 @@
 function raindrops(number)
-    sound = ""
-    if number % 3 == 0
-        sound *= "Pling"
+    if number % 3 == 0 && number % 5 != 0 && number % 7 != 0
+        return "Pling"
+    elseif number % 5 == 0 && number % 3 != 0 && number % 7 != 0
+        return "Plang"
+    elseif number % 7 == 0 && number % 3 != 0 && number % 5 != 0
+        return "Plong"
+    elseif number % 3 == 0 && number % 5 == 0 && number % 7 != 0
+        return "PlingPlang"
+    elseif number % 5 == 0 && number % 7 == 0 && number % 3 != 0
+        return "PlangPlong"
+    elseif number % 7 == 0 && number % 3 == 0 && number % 5 != 0
+        return "PlingPlong"
+    elseif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+        return "PlingPlangPlong"
+    else
+        return string(number)
     end
-
-    if number % 5 == 0
-        sound *= "Plang"
-    end
-
-    if number % 7 == 0
-        sound *= "Plong"
-    end
-
-    return sound == "" ? string(number) : sound
 
 end
