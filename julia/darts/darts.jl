4fb0ab2cbe82c51602e02ce133f1278c50dac868
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-24 20:47:45 +0700

    complete darts other way

--- julia/darts/darts.jl
+++ julia/darts/darts.jl
@@ -1,17 +1,29 @@
+const dart_radius = 10
 function score(x::Number, y::Number)::Int
     radius = sqrt(x * x + y * y)
-    if radius <= 1
+    # if radius <= 1
+    #     return 10
+    # end
+    #
+    # if radius <= 5
+    #     return 5
+    # end
+    #
+    # if radius <= 10
+    #     return 1
+    # end
+    #
+    # return 0
+
+    dist = dart_radius - radius
+    if dist < 0
+        return 0
+    elseif dist >= 9
         return 10
-    end
-
-    if radius <= 5
+    elseif dist >= 5
         return 5
-    end
-
-    if radius <= 10
+    else
         return 1
     end
-
-    return 0
 
 end
