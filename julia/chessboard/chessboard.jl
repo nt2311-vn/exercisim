1417b1ec712ed77c7a7dfa68f4364ee531db474b
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-26 21:29:46 +0700

    complete the range challenges

---
+++ julia/chessboard/chessboard.jl
@@ -0,0 +1,15 @@
+function rank_range()
+    return 1:8
+end
+
+function file_range()
+    return 'A':'H'
+end
+
+function ranks()
+    return collect(1:8)
+end
+
+function files()
+    return collect('A':'H')
+end
