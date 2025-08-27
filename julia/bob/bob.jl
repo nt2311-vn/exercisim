7ca233efd1c16f37ab5e3925b4760da182b23e0e
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-27 22:01:10 +0700

    complete bob challenge

---
+++ julia/bob/bob.jl
@@ -0,0 +1,21 @@
+function is_question(msg::String)::Bool
+    return strip(msg)[end] == '?'
+end
+
+function is_yell(msg::String)::Bool
+    return msg == uppercase(msg) && lowercase(msg) != msg
+end
+
+function bob(stimulus)
+    if strip(stimulus) == ""
+        return "Fine. Be that way!"
+    elseif is_yell(stimulus) && is_question(stimulus)
+        return "Calm down, I know what I'm doing!"
+    elseif is_yell(stimulus)
+        return "Whoa, chill out!"
+    elseif is_question(stimulus)
+        return "Sure."
+    else
+        return "Whatever."
+    end
+end
