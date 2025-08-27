95ace967f5ae71fb3963a017cd189d688ea133b0
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-27 22:21:17 +0700

    solved with bit manipulation

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,28 +1,40 @@
-function secret_handshake(code)
-    should_reverse, jump, close_eyes, double_blink, wink = split(bitstring(Int8(code))[4:8], "")
+function secret_handshake(n::Integer)
+    # should_reverse, jump, close_eyes, double_blink, wink = split(bitstring(Int8(code))[4:8], "")
+    #
+    # actions = []
+    #
+    # if wink == "1"
+    #     push!(actions, "wink")
+    # end
+    #
+    # if double_blink == "1"
+    #     push!(actions, "double blink")
+    # end
+    #
+    # if close_eyes == "1"
+    #     push!(actions, "close your eyes")
+    # end
+    #
+    # if jump == "1"
+    #     push!(actions, "jump")
+    # end
+    #
+    # if should_reverse == "1"
+    #     actions = reverse(actions)
+    # end
+    #
+    #
+    # return actions
 
-    actions = []
-
-    if wink == "1"
-        push!(actions, "wink")
+    actions = ["wink", "double blink", "close your eyes", "jump"]
+    result = String[]
+    for i in 1:4
+        if (n >> (i - 1)) & 1 == 1
+            push!(result, actions[i])
+        end
     end
-
-    if double_blink == "1"
-        push!(actions, "double blink")
+    if (n >> 4) & 1 == 1
+        reverse!(result)
     end
-
-    if close_eyes == "1"
-        push!(actions, "close your eyes")
-    end
-
-    if jump == "1"
-        push!(actions, "jump")
-    end
-
-    if should_reverse == "1"
-        actions = reverse(actions)
-    end
-
-
-    return actions
+    return result
 end
