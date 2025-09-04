594387f8d2e8f9612449358d250234808db38826
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-04 22:11:18 +0700

    solved the other way

--- julia/inventory-management/inventory-management.jl
+++ julia/inventory-management/inventory-management.jl
@@ -1,43 +1,31 @@
-function create_inventory(items)
-
+function create_inventory(items::Vector{String})::Dict{String, Int}
     stock = Dict{String, Int}()
-
-    for item in items
-        if !haskey(stock, item)
-            stock[item] = 0
-        end
-
-        stock[item] += 1
+    for item::String in items
+        stock[item] = get(stock, item, 0) + 1
     end
-
     return stock
 end
 
-function add_items(inventory, items)
+function add_items(inventory::Dict, items::Vector{String})::Dict{String, Int}
 
-    for item in items
-        if !haskey(inventory, item)
-            inventory[item] = 0
-        end
-
-        inventory[item] += 1
+    for item::String in items
+        inventory[item] = get(inventory, item, 0) + 1
     end
 
     return inventory
-
 end
 
-function decrement_items(inventory, items)
-    for item in items
-        if haskey(inventory, item) && inventory[item] > 0
+function decrement_items(inventory::Dict, items::Vector{String})::Dict
+
+    for item::String in items
+        if get(inventory, item, 0) > 0
             inventory[item] -= 1
         end
     end
-
     return inventory
 end
 
-function remove_item(inventory, item)
+function remove_item(inventory::Dict, item::String)::Dict
     if haskey(inventory, item)
         delete!(inventory, item)
     end
@@ -45,13 +33,6 @@ function remove_item(inventory, item)
     return inventory
 end
 
-function list_inventory(inventory)
-    inventory_vector = Pair{String, Int}[]
-    for key in keys(inventory)
-        if inventory[key] > 0
-            push!(inventory_vector, key => inventory[key])
-        end
-    end
-    sort!(inventory_vector, by = item -> item.first)
-    return inventory_vector
+function list_inventory(inventory::Dict)::Vector{Pair{String, Int}}
+    return sort([(k => v) for (k, v) in inventory if v > 0], by = x -> x.first)
 end
