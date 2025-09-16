40abd23b8486271e9a568e701f1d3d310fe586e0
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-16 21:50:43 +0700

    remove to do comments

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -7,12 +7,10 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
-# TODO: define your EXPECTED_BAKE_TIME (required) and PREPARATION_TIME (optional) constants below.
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
 
-# TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(elapsed_bake_time: int) -> int:
     """Calculate the bake time remaining.
 
@@ -27,12 +25,6 @@ def bake_time_remaining(elapsed_bake_time: int) -> int:
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: Define the 'preparation_time_in_minutes()' function below.
-# To avoid the use of magic numbers (see: https://en.wikipedia.org/wiki/Magic_number_(programming)), you should define a PREPARATION_TIME constant.
-# You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
-# This will make it easier to do calculations, and make changes to your code.
-
-
 def preparation_time_in_minutes(number_of_layers: int) -> int:
     """Calculation the preparation time in minutes
 
@@ -44,7 +36,6 @@ def preparation_time_in_minutes(number_of_layers: int) -> int:
     return PREPARATION_TIME * number_of_layers
 
 
-# TODO: define the 'elapsed_time_in_minutes()' function below.
 def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> int:
     """Calculate the elapsed cooking time.
 
@@ -58,7 +49,3 @@ def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> in
     """
 
     return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
-
-
-# TODO: Remember to go back and add docstrings to all your functions
-#  (you can copy and then alter the one from bake_time_remaining.)
