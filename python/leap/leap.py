ca091f080ce1f5d163b7cb4a7bf9f742173f192d
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-17 21:40:57 +0700

    using if branch to solve leap year

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -5,5 +5,7 @@ def leap_year(year: int) -> bool:
     :param year: int -the provided year
     :return: int - whether provided year is leap year or not
     """
+    if year % 400 == 0:
+        return True
 
-    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+    return year % 4 == 0 and year % 100 != 0
