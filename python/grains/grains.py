50b357f701e85043cea10681aa5a84ca2b484465
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-20 12:38:57 +0700

    complete the challenge on grains

---
+++ python/grains/grains.py
@@ -0,0 +1,9 @@
+def square(number: int) -> int:
+    if number <= 0 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+
+    return 2 ** (number - 1)
+
+
+def total() -> int:
+    return sum(square(n) for n in range(1, 65))
