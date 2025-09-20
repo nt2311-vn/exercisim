140284cf8f2ba26f2ed629e78bec19761ba694ef
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-20 12:50:35 +0700

    complete challenge in one liner

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -4,10 +4,4 @@ def is_armstrong_number(number: int) -> bool:
     if num_len == 1:
         return True
 
-    char_list = list(num_str)
-    total = 0
-
-    for char_num in char_list:
-        total += int(char_num) ** num_len
-
-    return total == number
+    return sum(int(char_num) ** num_len for char_num in list(num_str)) == number
