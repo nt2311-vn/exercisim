450b49ffa63e8395de40ff3f76849faea5a8eadc
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-20 14:57:43 +0700

    using tail-call recursive to solve

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,18 +1,11 @@
-def steps(number: int) -> int:
+def steps(number: int, count: int = 0) -> int:
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
 
     if number == 1:
-        return 0
+        return count
 
-    step = 0
-    x = number
-    while x != 1:
-        step += 1
+    if number % 2 == 0:
+        return steps(number // 2, count + 1)
 
-        if x % 2 == 0:
-            x = x // 2
-        else:
-            x = 3 * x + 1
-
-    return step
+    return steps(3 * number + 1, count + 1)
