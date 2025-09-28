1359306d2b5925d62edbe65dbd0dedf36a07c1e1
ntcachyos <nguyentoan231196@gmail.com>
2025-09-28 16:25:04 +0700

    complete perfect numbers check number in aliquot sum

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -8,9 +8,6 @@ def classify(number: int) -> str:
     if number <= 0:
         raise ValueError("Classification is only possible for positive integers.")
 
-    if number == 1:
-        return "deficient"
-
     compare = number - aliquot_sum(number)
     print(aliquot_sum(number))
     if compare == 0:
@@ -23,5 +20,7 @@ def classify(number: int) -> str:
 
 
 def aliquot_sum(number: int) -> int:
+    if number < 2:
+        return 0
     max_aliquot = number // 2
     return sum(num for num in range(2, max_aliquot + 1) if number % num == 0) + 1
