82e2370dac54424906a7c1c9775e7a3bc3493980
ntcachyos <nguyentoan231196@gmail.com>
2025-09-29 21:45:06 +0700

    complete resitor color challenge

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,39 @@
+def color_code(color: str) -> int:
+    match color:
+        case "black":
+            return 0
+        case "brown":
+            return 1
+        case "red":
+            return 2
+        case "orange":
+            return 3
+        case "yellow":
+            return 4
+        case "green":
+            return 5
+        case "blue":
+            return 6
+        case "violet":
+            return 7
+        case "grey":
+            return 8
+        case "white":
+            return 9
+        case _:
+            raise ValueError("unpextor color value")
+
+
+def colors() -> list[str]:
+    return [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
