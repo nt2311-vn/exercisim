026a8f82200dc85e854f2431f7fa22adea07dc4e
ntcachyos <nguyentoan231196@gmail.com>
2025-10-03 21:04:35 +0700

    revere a stringn using for loop

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,6 +1,11 @@
 class ReverseString {
 
   String reverse(String inputString) {
-    return new StringBuilder(inputString).reverse().toString();
+    String reversed = "";
+
+    for (int i = 0; i < inputString.length(); i++) {
+      reversed = inputString.charAt(i) + reversed;
+    }
+    return reversed;
   }
 }
