3a872ef36ad37268e97db4c9e794235f24f43956
ntcachyos <nguyentoan231196@gmail.com>
2025-10-19 22:28:37 +0700

    complete challenge snatch them all set

--- java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -1,25 +1,84 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 class GottaSnatchEmAll {
 
-    static Set<String> newCollection(List<String> cards) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.newCollection() method");
+  static Set<String> newCollection(List<String> cards) {
+    Set<String> collections = new HashSet<>();
+
+    for (String card : cards) {
+      collections.add(card);
     }
 
-    static boolean addCard(String card, Set<String> collection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.addCard() method");
+    return collections;
+  }
+
+  static boolean addCard(String card, Set<String> collection) {
+    if (!collection.contains(card)) {
+      collection.add(card);
+      return true;
     }
 
-    static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.canTrade() method");
+    return false;
+  }
+
+  static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
+    if (myCollection.isEmpty() || theirCollection.isEmpty()) {
+      return false;
     }
 
-    static Set<String> commonCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.commonCards() method");
+    int myNotExistCard = 0;
+    int theirNotExistCard = 0;
+
+    for (String card : theirCollection) {
+      if (!myCollection.contains(card)) {
+        myNotExistCard++;
+      }
     }
 
-    static Set<String> allCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.allCards() method");
+    for (String card : myCollection) {
+      if (!theirCollection.contains(card)) {
+        theirNotExistCard++;
+      }
     }
+
+    return myNotExistCard >= 1 && theirNotExistCard >= 1;
+  }
+
+  static Set<String> commonCards(List<Set<String>> collections) {
+    HashMap<String, Integer> common = new HashMap<>();
+
+    for (Set<String> collection : collections) {
+      for (String card : collection) {
+        int currentAppear = common.getOrDefault(card, 0);
+        common.put(card, currentAppear + 1);
+      }
+    }
+
+    Set<String> result = new HashSet<>();
+    int requiredCount = collections.size();
+
+    for (Map.Entry<String, Integer> entry : common.entrySet()) {
+      if (entry.getValue() == requiredCount) {
+        result.add(entry.getKey());
+      }
+    }
+
+    return result;
+  }
+
+  static Set<String> allCards(List<Set<String>> collections) {
+    Set<String> cards = new HashSet<>();
+
+    for (Set<String> collection : collections) {
+      for (String card : collection) {
+        cards.add(card);
+      }
+    }
+
+    return cards;
+  }
 }
