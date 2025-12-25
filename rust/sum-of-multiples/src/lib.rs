982935229d698bcbdec0d4c383e1da3c81bc79b8
nt2311-vn <nguyentoan231196@gmail.com>
2025-12-25 17:26:44 +0700

    using multiple combination of filter iter and any

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,17 +1,5 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut multiples = HashSet::new();
-
-    for &factor in factors {
-        if factor == 0 {
-            continue;
-        }
-
-        for multiple in (factor..limit).step_by(factor as usize) {
-            multiples.insert(multiple);
-        }
-    }
-
-    multiples.iter().sum()
+    (1..limit)
+        .filter(|&n| factors.iter().filter(|&&f| f != 0).any(|&f| n % f == 0))
+        .sum()
 }
