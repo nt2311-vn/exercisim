665a6509e120e51797d9c2dc046badc72729f15f
nt2311-vn <nguyentoan231196@gmail.com>
2025-12-30 17:42:26 +0700

    using combination windows.map().collect

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,12 +1,8 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    if digits.len() == 0 || digits.len() < len {
-        return result;
-    }
-
-    for i in 0..=(digits.len() - len) {
-        result.push((digits[i..len + i]).to_string());
-    }
-
-    result
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|w| w.iter().collect::<String>())
+        .collect()
 }
