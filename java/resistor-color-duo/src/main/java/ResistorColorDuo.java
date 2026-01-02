744fd241b29c93856d8032b20e7e8453c03f3eb3
nt2311-vn <nguyentoan231196@gmail.com>
2026-01-02 15:48:55 +0700

    complete resitor duo challenge using simple list

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -2,23 +2,12 @@ import java.util.Arrays;
 import java.util.List;
 
 class ResistorColorDuo {
-  private final String[] allColors = {
-    "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
-  };
-
-  String colorValue(String color) {
-    return Integer.toString(Arrays.asList(allColors).indexOf(color));
-  }
+  private final List<String> allColors =
+      Arrays.asList(
+          "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
 
   int value(String[] colors) {
-    String result = "";
 
-    List<String> listOfTwo = Arrays.asList(colors).subList(0, 2);
-
-    for (int i = 0; i < listOfTwo.size(); i++) {
-      result += colorValue(listOfTwo.get(i));
-    }
-
-    return Integer.parseInt(result);
+    return allColors.indexOf(colors[0]) * 10 + allColors.indexOf(colors[1]);
   }
 }
