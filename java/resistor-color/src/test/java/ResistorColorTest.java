99e83022f6038d05f9b22c712817f9908c3d00f2
nt2311-vn <nguyentoan231196@gmail.com>
2026-01-02 15:19:52 +0700

    complete challenge resitor color

---
+++ java/resistor-color/src/test/java/ResistorColorTest.java
@@ -0,0 +1,42 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ResistorColorTest {
+
+  private ResistorColor resistorColor;
+
+  @BeforeEach
+  public void setup() {
+    resistorColor = new ResistorColor();
+  }
+
+  @Test
+  @DisplayName("Black")
+  public void testBlackColorCode() {
+    assertThat(resistorColor.colorCode("black")).isEqualTo(0);
+  }
+
+  @Test
+  @DisplayName("white")
+  public void testWhiteColorCode() {
+    assertThat(resistorColor.colorCode("white")).isEqualTo(9);
+  }
+
+  @Test
+  @DisplayName("Orange")
+  public void testOrangeColorCode() {
+    assertThat(resistorColor.colorCode("orange")).isEqualTo(3);
+  }
+
+  @Test
+  @DisplayName("Colors")
+  public void testColors() {
+    assertThat(resistorColor.colors())
+        .containsExactly(
+            "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey",
+            "white");
+  }
+}
