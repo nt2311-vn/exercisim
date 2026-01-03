0a7c9bad09198e66e7cfcff0daeb12905dea585c
ntcachyos <nguyentoan231196@gmail.com>
2026-01-03 20:10:37 +0700

    complete the challenge pangram using int stream distince

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,22 +1,6 @@
-import java.util.HashSet;
-
 public class PangramChecker {
 
   public boolean isPangram(String input) {
-    if (input.isEmpty()) {
-      return false;
-    }
-
-    HashSet<Character> alphabets = new HashSet<>();
-
-    final char[] str = input.toLowerCase().replaceAll(" ", "").toCharArray();
-
-    for (char c : str) {
-      if (Character.isAlphabetic(c)) {
-        alphabets.add(c);
-      }
-    }
-
-    return alphabets.size() == 26;
+    return input.toLowerCase().replaceAll("[^a-z]", "").chars().distinct().count() == 26;
   }
 }
