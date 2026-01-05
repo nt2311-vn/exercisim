292912c0736055ab21a9489d8aaa81d4bce0b858
nt2311-vn <nguyentoan231196@gmail.com>
2026-01-05 15:59:17 +0700

    complete count one in binary representation of a number

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,5 +1,3 @@
 pub fn egg_count(display_value: u32) -> usize {
-    let bit_str: String = format!("{:b}", display_value);
-
-    bit_str.chars().filter(|x| *x == '1').count()
+    display_value.count_ones() as usize
 }
